fef5b69f32fd6ad8238eba19ee54547de619897e
Ivan Berman <bermanivan42@gmail.com>
2025-12-24 12:52:57 +0200

    Remove RWS registration usage from config flow
    - Since Libp2p is no longer used, the whole section with
    RWSRegistrationManager is removed.
    - To enter integrator address and Pinata credentials new forms are
    introduced, which will store all data in the HA ConfigEntry.
    - In future it is better to move creds to separate protected
    storage.

--- custom_components/robonomics_report_service/config_flow.py
+++ custom_components/robonomics_report_service/config_flow.py
@@ -1,23 +1,34 @@
-import typing as tp
 import logging
+from typing import Any
 
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlowResult
-from tenacity import retry, stop_after_attempt, wait_fixed, after_log
 
-from .const import DOMAIN, CONF_EMAIL, CONF_SENDER_SEED
+from .const import (
+    DOMAIN,
+    CONF_SENDER_SEED,
+    CONF_INTEGRATOR_ADDRESS,
+    CONF_PINATA_SECRET,
+    CONF_PINATA_PUBLIC,
+    )
 
 from .robonomics import Robonomics
-from .rws_registration import RWSRegistrationManager
-from .libp2p import LibP2P
 
 _LOGGER = logging.getLogger(__name__)
+
+STEP_USER_DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_INTEGRATOR_ADDRESS): str,
+        vol.Required(CONF_PINATA_SECRET): str,
+        vol.Required(CONF_PINATA_PUBLIC): str,
+    }
+)
 
 
 class ReportServiceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """
-    Handle a config flow for the Report Service.
+    Handle a config flow for the Report Service during integration setup.
 
     The class object exists only for the duration of the setup wizard,
     and the result is a ConfigEntry that lives permanently.
@@ -28,12 +39,15 @@ class ReportServiceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     def __init__(self):
-        self.user_data = {}
         self.seed_saved = False
+        self.user_data = {}
 
-    async def async_step_user(self, user_input=None) -> ConfigFlowResult:
+    async def async_step_user(
+            self,
+            user_input: dict[str, Any] | None = None
+            ) -> ConfigFlowResult:
         """
-        Handle the initial step of the configuration.
+        The initial step of the configuration
         """
 
         # Since it is needed exactly one integration instance, then assign
@@ -44,31 +58,38 @@ class ReportServiceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         # Abort the flow if a config entry with the same unique ID exists
         self._abort_if_unique_id_configured()
 
+        # Show the form to enter intergrator and Pinata data
+        # if it hasn't already been done, then save data in user_data
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+        self.user_data = user_input
+
         sender_seed = Robonomics.generate_seed()
         self.user_data[CONF_SENDER_SEED] = sender_seed
         return await self.async_step_seed()
 
-    async def async_step_seed(self, user_input: dict[str, tp.Any] | None = None):
+    async def async_step_seed(
+            self,
+            user_input: dict[str, Any] | None = None
+            ) -> ConfigFlowResult:
+        """
+        Show the seed to user and configure Robonomics
+        """
+
+        # Show the form with the seed if it hasn't already been done
         if not self.seed_saved:
             self.seed_saved = True
             return self.async_show_form(
                 step_id="seed",
                 data_schema=vol.Schema({}),
-                description_placeholders={"seed": self.user_data[CONF_SENDER_SEED]},
-            )
-        else:
-            robonomics = Robonomics(
-                self.hass,
-                self.user_data[CONF_SENDER_SEED],
-            )
-            await robonomics.setup()
-            libp2p = LibP2P(robonomics.sender_address)
-            await self.register_with_retry(robonomics, libp2p)
-            return self.async_create_entry(
-                title="Robonomics Report Service", data=self.user_data
+                description_placeholders={
+                    "seed": self.user_data[CONF_SENDER_SEED]
+                    },
             )
 
-    @retry(stop=stop_after_attempt(4), wait=wait_fixed(4), after=after_log(_LOGGER, logging.WARNING))
-    async def register_with_retry(self, robonomics: Robonomics, libp2p: LibP2P):
-        await RWSRegistrationManager.register(self.hass, robonomics, libp2p, self.user_data[CONF_EMAIL])
-
+        # Save config as persistent ConfigEntry in HA
+        return self.async_create_entry(
+            title="Robonomics Report Service", data=self.user_data
+        )
