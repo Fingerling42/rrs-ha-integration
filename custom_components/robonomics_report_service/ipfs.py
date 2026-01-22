b4bae221ca5d967439c229687809870f35193444
Ivan Berman <bermanivan42@gmail.com>
2026-01-22 20:16:31 +0200

    Refactor ipfs.py
    - The types returned from functions have been clarified.
    - Added more robust handling of responses from Pinata.
    - Unpin can accept both JSON string and dict.

--- custom_components/robonomics_report_service/ipfs.py
+++ custom_components/robonomics_report_service/ipfs.py
@@ -7,74 +7,162 @@ from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
 from pinatapy import PinataPy
 
-from .utils import async_load_from_store
-from .const import STORAGE_CREDENTIALS, CONF_PINATA_PUBLIC, CONF_PINATA_SECRET
+from .utils.ha_storage import async_load_from_store
+from .const import STORAGE_NAME, CONF_PINATA_PUBLIC, CONF_PINATA_SECRET
+
+IpfsHashes = dict[str, str]
 
 _LOGGER = logging.getLogger(__name__)
 
 class PinataKeysRewoked(HomeAssistantError):
     """Pinata API Key has been revoked"""
 
-
 class IPFS:
+    """Class for handling IPFS and Pinata functionality"""
+
     def __init__(self, hass: HomeAssistant):
         self.hass = hass
 
-    async def pin_to_pinata(self, dirname: str) -> tp.Optional[str]:
+    async def pin_to_pinata(self, dir_name: str) -> tp.Optional[IpfsHashes]:
+        """Upload and pin directory to Pinata and get its IPFS hash"""
         pinata = await self._get_pinata_with_creds()
-        if pinata is not None:
-            ipfs_hash = await self.hass.async_add_executor_job(
-                self._pin_to_pinata, dirname, pinata
-            )
-            return ipfs_hash
 
-    async def unpin_from_pinata(self, ipfs_hashes_dict: str | dict) -> tp.Optional[str]:
+        if pinata is None:
+            return None
+
+        return await self.hass.async_add_executor_job(
+            self._pin_to_pinata,
+            dir_name,
+            pinata
+        )
+
+    async def unpin_from_pinata(
+        self,
+        ipfs_hashes: str | IpfsHashes
+    ) -> None:
+        """Unpin IPFS file from Pinata"""
+
         pinata = await self._get_pinata_with_creds()
-        if isinstance(ipfs_hashes_dict, str):
-            ipfs_hashes_dict = json.loads(ipfs_hashes_dict)
-        if pinata is not None:
-            await self.hass.async_add_executor_job(
-                self._unpin_from_pinata, ipfs_hashes_dict, pinata
-            )
+
+        if pinata is None:
+            return
+
+        loaded: object
+        if isinstance(ipfs_hashes, str):
+            try:
+                loaded = json.loads(ipfs_hashes)
+            except json.JSONDecodeError:
+                _LOGGER.warning("Unexpected unpin payload: invalid JSON")
+                return
+        else:
+            loaded = ipfs_hashes
+
+        if not isinstance(loaded, dict):
+            _LOGGER.warning("Unexpected unpin payload type: %s", type(loaded))
+            return
+
+        ipfs_hashes_typed = tp.cast(IpfsHashes, loaded)
+
+        await self.hass.async_add_executor_job(
+            self._unpin_from_pinata,
+            ipfs_hashes_typed,
+            pinata
+        )
 
     async def _get_pinata_with_creds(self) -> tp.Optional[PinataPy]:
-        storage_data = await async_load_from_store(self.hass, STORAGE_CREDENTIALS)
-        if CONF_PINATA_PUBLIC in storage_data and CONF_PINATA_SECRET in storage_data:
+        storage_data = await async_load_from_store(self.hass, STORAGE_NAME)
+        if (
+            CONF_PINATA_PUBLIC in storage_data
+            and CONF_PINATA_SECRET in storage_data
+        ):
             return PinataPy(
-                storage_data[CONF_PINATA_PUBLIC], storage_data[CONF_PINATA_SECRET]
+                storage_data[CONF_PINATA_PUBLIC],
+                storage_data[CONF_PINATA_SECRET]
             )
+        return None
 
-    def _pin_to_pinata(self, dirname: str, pinata: PinataPy) -> tp.Optional[str]:
-        res = None
-        dict_with_hashes = {}
+    def _pin_to_pinata(
+        self,
+        dir_name: str,
+        pinata: PinataPy
+    ) -> tp.Optional[IpfsHashes]:
 
-        _LOGGER.debug(f"tmp dir: {dirname}")
-        file_names = [
-            f
-            for f in os.listdir(dirname)
-            if os.path.isfile(os.path.join(dirname, f))
-        ]
-        _LOGGER.debug(f"file names: {file_names}")
+        dict_with_hashes: IpfsHashes = {}
+
+        try:
+            file_names = [
+                f for f in os.listdir(dir_name)
+                if os.path.isfile(os.path.join(dir_name, f))
+            ]
+        except OSError as e:
+            _LOGGER.error("Can't list dir %s: %s", dir_name, e)
+            return None
+
         for file in file_names:
-            path_to_file = f"{dirname}/{file}"
-            res = pinata.pin_file_to_ipfs(path_to_file, save_absolute_paths=False)
-            ipfs_hash: tp.Optional[str] = res.get("IpfsHash")
-            if ipfs_hash:
-                _LOGGER.debug(f"Added file {file} to Pinata. Hash is: {ipfs_hash}")
-                dict_with_hashes[file] = ipfs_hash
-            elif res['status'] == 403 and "API_KEY_REVOKED" in res["text"]:
-                _LOGGER.warning("Pinata keys was revoked")
-                raise PinataKeysRewoked
-            else:
-                _LOGGER.error(f"Can't pin to pinata with responce: {res}")
-        _LOGGER.debug(f"Dict with hashes: {dict_with_hashes}")
-        if dict_with_hashes:
-            return dict_with_hashes
+            path_to_file = os.path.join(dir_name, file)
 
-    def _unpin_from_pinata(self, ipfs_hashes_dict: tp.Dict, pinata: PinataPy) -> None:
-        _LOGGER.debug(f"Start removing pins: {ipfs_hashes_dict}")
-        for key in ipfs_hashes_dict:
-            current_hash: str = ipfs_hashes_dict[key]
+            try:
+                res = pinata.pin_file_to_ipfs(
+                    path_to_file,
+                    save_absolute_paths=False
+                )
+            except Exception as e:
+                _LOGGER.error(
+                    "Pinata pin_file_to_ipfs failed for %s: %s",
+                    file,
+                    e
+                )
+                continue
+
+            if not isinstance(res, dict):
+                _LOGGER.error(
+                    "Can't pin to Pinata, unexpected response: %s",
+                    res
+                )
+                continue
+
+
+            ipfs_hash = res.get("IpfsHash")
+
+            if isinstance(ipfs_hash, str) and ipfs_hash:
+                _LOGGER.debug(
+                    "Added file %s to Pinata. Hash is: %s",
+                    file,
+                    ipfs_hash
+                )
+                dict_with_hashes[file] = ipfs_hash
+                continue
+
+            status = res.get("status")
+            text = res.get("text", "")
+
+            if (
+                status == 403 and isinstance(text, str)
+                and "API_KEY_REVOKED" in text
+                ):
+                _LOGGER.warning("Pinata keys were revoked")
+                raise PinataKeysRewoked()
+
+            _LOGGER.error("Can't pin to pinata with responce: %s", res)
+
+        return dict_with_hashes or None
+
+    def _unpin_from_pinata(
+            self,
+            ipfs_hashes_dict: IpfsHashes,
+            pinata: PinataPy
+        ) -> None:
+        _LOGGER.debug("Start removing Pinata pins: %s", ipfs_hashes_dict)
+
+        for _, current_hash in ipfs_hashes_dict.items():
             if isinstance(current_hash, str) and current_hash.startswith("Qm"):
-                res = pinata.remove_pin_from_ipfs(current_hash)
-                _LOGGER.debug(f"Remove response for pin {current_hash}: {res}")
+                try:
+                    res = pinata.remove_pin_from_ipfs(current_hash)
+                except Exception as e:
+                    _LOGGER.warning("Failed to unpin %s: %s", current_hash, e)
+                    continue
+                _LOGGER.debug(
+                    "Remove response for pin %s: %s",
+                    current_hash,
+                    res
+                )
